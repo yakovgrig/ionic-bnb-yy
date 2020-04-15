3bc80806427a0fdfc26f41cafb3b0d1cb8fde8bf
Yakov Grig <yakov.grig@gmail.com>
2020-04-15 13:43:53 +0300

    first commit

---
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Place } from '../../place.model';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesService } from '../../places.service';
+import { NavController, ModalController } from '@ionic/angular';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
+
+@Component({
+  selector: 'app-place-detail',
+  templateUrl: './place-detail.page.html',
+  styleUrls: ['./place-detail.page.scss'],
+})
+export class PlaceDetailPage implements OnInit {
+
+  place: Place;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private placesService: PlacesService, 
+    private navController:NavController,
+    private modalController: ModalController
+
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+
+      if (!paramMap.has("placeId")){
+
+        this.navController.navigateBack('/places/tabs/offers');
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
+    });
+  }
+  
+  onBookPlace(){
+    
+    this.modalController.create({
+      component: CreateBookingComponent
+      , componentProps: { selectedPlace: this.place }
+      , animated: true
+      , swipeToClose: true
+      }).then(modalEl => {
+
+        modalEl.present();
+        return modalEl.onDidDismiss();
+
+      }).then(resultData =>{
+
+        console.log(resultData);
+        if (resultData.role === "confirm"){
+
+          console.log("BOOKED!");
+        }
+      });
+  }
+}
---
+++ src/app/places/offers/offers.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from './../place.model';
+
+@Component({
+  selector: 'app-offers',
+  templateUrl: './offers.page.html',
+  styleUrls: ['./offers.page.scss'],
+})
+export class OffersPage implements OnInit {
+
+  constructor(private placesService: PlacesService) { }
+
+  offers: Place [];
+  
+  ngOnInit() {
+      this.offers = this.placesService.places;
+  }
+
+}
